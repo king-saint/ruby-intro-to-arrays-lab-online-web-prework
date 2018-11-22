55e948aa172cd04ab1b9956df8bf0c13211888cc
Wendi Chen <epanistamai.proles@gmail.com>
2018-11-22 18:49:57 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -36,7 +36,8 @@ def last_element_with_array_methods
   south_east_asia.last
 end
 
-def length_of_array
-  programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
-  programming_languages.length
+programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
+def length_of_array(array)
+  
+  array.length
 end
